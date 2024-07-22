ea9f5ea394d122f63016966faf3506ff61706a30
diegoseg_15 <diegofersv@gmail.com>
2024-07-21 21:30:31 -0500

    :fire: Removed WhatsApp button and related code

--- src/Components/Presentation/Presentation.jsx
+++ src/Components/Presentation/Presentation.jsx
@@ -60,33 +60,6 @@ export function Presentation() {
         <li>
           <button
             className="flex dark:font-normal font-bold hover:text-sky-600 md:pr-10 pr-5 p-1"
-            onMouseEnter={(event) => mediaCapture(event, "whatsapp")}
-            onMouseLeave={(event) => mediaCapture(event, null)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-brand-whatsapp"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
-              <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
-            </svg>
-            {nameMedia === "whatsapp" && (
-              <p className="ml-1 md:block hidden">+593 97 878 2596</p>
-            )}
-          </button>
-        </li>
-        <li>
-          <button
-            className="flex dark:font-normal font-bold hover:text-sky-600 md:pr-10 pr-5 p-1"
             onMouseEnter={(event) => mediaCapture(event, "tiktok")}
             onMouseLeave={(event) => mediaCapture(event, null)}
           >
@@ -132,7 +105,7 @@ export function Presentation() {
               <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
             </svg>
             {nameMedia === "github" && (
-              <p className="ml-1 md:block hidden">diegoseg_15</p>
+              <p className="ml-1 md:block hidden">diegoseg15</p>
             )}
           </button>
         </li>
