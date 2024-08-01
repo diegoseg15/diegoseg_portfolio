f706fd848d59743b510c3ebd47123d308b6b0f53
diegoseg_15 <diegofersv@gmail.com>
2024-08-01 11:37:57 -0500

    :fire: Removed app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ClientRoutes from "./Routes/ClientRoutes";
-import "./App.css";
 
 export default function App() {
   return (
