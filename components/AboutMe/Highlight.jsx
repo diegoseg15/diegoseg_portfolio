a0a58c3af0824c573ef8725cf1766b9883c83a2a
diegoseg15 <diegofersv@gmail.com>
2026-01-19 17:22:07 -0500

    :dizzy: agregar animaciones al componente Highlight

--- components/AboutMe/Highlight.jsx
+++ components/AboutMe/Highlight.jsx
@@ -1,9 +1,20 @@
+import { motion } from "framer-motion";
 
 export function Highlight({ icon, text }) {
   return (
-    <div className="flex items-center gap-3 p-4 rounded-md bg-slate-100 dark:bg-slate-800 hover:scale-[1.03] transition">
+    <motion.div
+      initial={{ opacity: 0, y: 16 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.7, ease: "easeOut" }}
+      className="
+        flex items-center gap-3 p-4 rounded-md
+        bg-slate-100 dark:bg-slate-800
+        hover:scale-[1.05] transition
+      "
+    >
       <div className="text-sky-600">{icon}</div>
       <span>{text}</span>
-    </div>
+    </motion.div>
   );
 }
