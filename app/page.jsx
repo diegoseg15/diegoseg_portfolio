42746f70f7052e2ae53d582c487f658a6c587671
diegoseg15 <diegofersv@gmail.com>
2026-01-18 23:14:02 -0500

    :sparkles: agregar secciones Banner, AboutMe y Skills a la página principal

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,13 @@
-import Image from "next/image";
-import { HeaderComponent } from "@/components/Header/HeaderComponent";
-import { BannerComponent } from "@/components/Banner/BannerComponent";
-import { FooterComponent } from "@/components/Footer/FooterComponent";
-
+import Banner from "@/components/Banner"
+import AboutMe from "@/components/AboutMe"
+import Skills from "@/components/Skills"
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col">
-      <HeaderComponent />
-
-      {/* Banner ocupa el espacio disponible */}
-      <div className="z-0">
-        <BannerComponent />
-      </div>
-
-      <FooterComponent />
+    <main className="w-screen h-screen overflow-y-auto overflow-x-hidden childbarscroll" id="scroll-root">
+      <Banner />
+      <AboutMe/>
+      <Skills/>
     </main>
   );
 }
-
