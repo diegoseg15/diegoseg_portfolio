bd8a74a39d19ec35ac1a55f259a963e898f06f89
diegoseg15 <diegofersv@gmail.com>
2026-01-19 21:56:15 -0500

    :sparkles: agregar sección Projects y ajustar espaciado entre componentes

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,17 @@
 import Banner from "@/components/Banner/Banner"
 import AboutMe from "@/components/AboutMe/AboutMe"
 import Skills from "@/components/Skill/Skill"
+import Projects from "@/components/Projects/Projects";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen overflow-y-auto overflow-x-hidden childbarscroll" id="scroll-root">
       <Banner />
-      <AboutMe/>
-      <Skills/>
+      <div className="lg:space-y-28 space-y-16 lg:pt-28 pt-16">
+        <AboutMe />
+        <Skills />
+        <Projects />
+      </div>
     </main>
   );
 }
