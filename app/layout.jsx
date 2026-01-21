8aae4c10bd3fe1f0f8518ec435754ab8c05a88f2
diegoseg15 <diegofersv@gmail.com>
2026-01-21 15:55:12 -0500

    :lipstick: personalizar scrollbar responsivo del contenedor principal

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  childbarscroll`}
       >
         <Header />
         {children}
